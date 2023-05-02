1ef94fda4037b8790a9c84b98d7058991c7ed776
jjuric21 <jjuric21@foi.hr>
2023-05-02 20:47:45 +0200

    drugi commit

---
+++ Evaluation Manager/Repositories/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Evaluation_Manager.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories {
+    public class StudentRepository 
+        {
+        public static Student GetStudent(int id)
+            {
+
+        }
+
+    }
+}
