83c0d96a053009ce8308f662b5736aed1700e923
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-15 20:18:44 +0200

    Forma za evaluacije povezana sa repozitorijem

--- PI_vjezbe_EM/Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.cs
+++ PI_vjezbe_EM/Evaluation Manager DS/Evaluation Manager DS/FrmEvaluation.cs
@@ -13,6 +13,9 @@ using System.Windows.Forms;
 namespace Evaluation_Manager_DS {
     public partial class FrmEvaluation : Form {
         private Student student;
+
+        public Student SelectedStudent { get => student;  set => student = value; }
+
         public FrmEvaluation(Models.Student selectedStudent) {
             InitializeComponent();
             student = selectedStudent;
@@ -37,6 +40,21 @@ namespace Evaluation_Manager_DS {
            currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+
+
+            var evaluation = EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaluation != null) {
+                txtTeacher.Text = evaluation.Evaluator.TosString();
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+                numPoints.Value = evaluation.Points;
+            } else { 
+            txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+                         
+
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
